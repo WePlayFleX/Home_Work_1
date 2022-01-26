f7ce2558b38c488ed2fa45eef33b389fb0547502
WePlayFleX <alex.kamai@ya.ru>
2022-01-26 23:35:57 +0300

    Добавьте файлы проекта.

---
+++ Home_Work_1.cs
@@ -0,0 +1,48 @@
+﻿using cs = System.Console;
+using cv = System.Convert;
+
+
+namespace Home_Work_1
+{
+    class Home_Work_1
+    {
+        public static void Main()
+        {
+            cs.WriteLine("Enter the temperature in degrees Celsius :");
+            double temp = cv.ToDouble(cs.ReadLine());
+            double res = Implementation.Convert_Temperature(temp);
+            cs.WriteLine("Temperature in degrees Fahrenheit - " + res);
+
+
+            cs.WriteLine("Enter the amount in rubles BYN :");
+            double BYN = cv.ToDouble(cs.ReadLine());
+            double EUR = Implementation.Currency_Convert(BYN);
+            cs.WriteLine("Amount in Euro EUR - " + EUR);
+
+
+            cs.WriteLine("Enter the number :");
+            int num = cv.ToInt32(cs.ReadLine());
+            int reverse = Implementation.Reverse_Number(num);
+            cs.WriteLine("Number after reverse - " + reverse);
+
+
+            cs.WriteLine("Enter the number of hours :");
+            double numb = cv.ToInt32(cs.ReadLine());
+            double result = Implementation.Hours_to_Second(numb);
+            cs.WriteLine("Number of seconds - " + result);
+
+
+            cs.WriteLine("Enter there numbers");
+
+            double a = cv.ToDouble(cs.ReadLine());
+            double b = cv.ToDouble(cs.ReadLine());
+            double c = cv.ToDouble(cs.ReadLine());
+            double d = cv.ToDouble(cs.ReadLine());
+            result = Implementation.Average(a, b, c, d);
+            cs.WriteLine("Average - " + result);
+
+
+            cs.ReadKey();
+        }
+    }
+}
---
+++ Implementation.cs
@@ -0,0 +1,51 @@
+﻿namespace Home_Work_1
+{
+    class Implementation
+    {
+        public static double Convert_Temperature(double temp)
+        {
+            return ((temp * 1.8) + 32);
+        }
+
+
+        public static double Currency_Convert(double summ)
+        {
+            return (summ * 0.3377);
+        }
+
+
+        public static int Reverse_Number(int num)
+        {
+            bool mF = false;
+            if ((mF = num < 0))
+            {
+                num = -num;
+            }
+
+            int tmp = 0;
+            while (num != 0)
+            {
+                tmp = 10 * tmp + num % 10;
+                num /= 10;
+            }
+            return (mF ? -tmp : tmp);
+        }
+
+
+        public static double Hours_to_Second(double num)
+        {
+            return (num * 3600);
+        }
+
+
+        public static double Average(double a, double b, double c, double d)
+        {
+            return ((a + b + c + d) / 4);
+        }
+
+
+
+    }
+
+    
+}
